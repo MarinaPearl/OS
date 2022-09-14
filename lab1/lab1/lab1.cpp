d2c41cad1dad2a2bde9953e9c2d1e4a7b27c2241
marina <marina.demchuk.01@bk.ru>
2022-09-14 17:29:34 +0700

    add lab1

--- lab1/lab1/lab1.cpp
+++ lab1/lab1/lab1.cpp
@@ -1,34 +1,35 @@
 ﻿#include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include <pthread.h> 
 #include <string.h>
 #define SUCCESS 0
-typedef struct argumentsForPrintText {
-	const char* text;
-	int count;
-} argumentsForPrintTextInThread;
-void* printText(void* args) {
-	argumentsForPrintTextInThread* value = (argumentsForPrintTextInThread*)args;
-	for (int i = 0; i < value->count; ++i) {
-		printf("%s\n", value->text);
-	}
-	return NULL;
+typedef struct argumentsForFunction {
+    const char* text;
+    int count;
+} argumetsForFunctionInThread;
+void* printTextInThread(void* args) {
+    argumetsForFunctionInThread* value = (argumetsForFunctionInThread*)args;
+    for (int i = 0; i < value->count; ++i) {
+        printf("%s\n", value->text);
+    }
+    return NULL;
 }
 void printError(int valueError, const char* msg) {
-	fprintf(stderr, "%s cause : %s\n", msg, strerror(valueError));
-	exit(EXIT_FAILURE);
+    fprintf(stderr, "%s cause : %s\n", msg, strerror(valueError));
+    exit(EXIT_FAILURE);
 }
 int main() {
-	pthread_t ntid;
-
-	argumentsForPrintTextInThread newThread = { "Hello, I'm new thread\n", 10 };
-	argumentsForPrintTextInThread mainThread = { "Hello, I'm main thread\n", 10 };
-
-	int err = pthread_create(&ntid, NULL, printText, (void*)&newThread);
-
-	if (err != SUCCESS) {
-		printError(err, "unable to create thread");
-	}
-	printText((void*)&mainThread);
-	pthread_exit(NULL);
+    pthread_t ntid;
+    argumetsForFunctionInThread newThread = { "Hello, I'm new thread\n", 10 };
+    argumetsForFunctionInThread mainThread = { "Hello, I'm main thread\n", 10 };
+    int err = pthread_create(&ntid, NULL, printTextInThread, (void*)&newThread);
+    if (err != SUCCESS) {
+        printError(err, "unable to create thread");
+    }
+    err = pthread_join(ntid, NULL);
+    if (err != SUCCESS) {
+        printError(err, "it is impossible to continue the main stream");
+    }
+    printTextInThread((void*)&mainThread);
+    return EXIT_SUCCESS;
 }
