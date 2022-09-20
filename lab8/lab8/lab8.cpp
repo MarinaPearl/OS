d25bdf30bb6e34571ed0eb75634a1291e3d97533
marina <marina.demchuk.01@bk.ru>
2022-09-20 23:04:09 +0700

    add case

--- lab8/lab8/lab8.cpp
+++ lab8/lab8/lab8.cpp
@@ -12,167 +12,167 @@
 #define JOIN_SUCCESS 0
 
 typedef struct inputArguments {
-	int countThread;
-	int countIterations;
+    int countThread;
+    int countIterations;
 } inputArguments;
 
 typedef struct argumentsForFunctionInThread {
-	int startIteration;
-	int endIteration;
-	double partialSum;
+    int startIteration;
+    int endIteration;
+    double partialSum;
 } argumentsForFunctionInThread;
 
 enum parsInputArguments {
-	inputArguments_SUCCESS,
-	inputArguments_WRONG_COUNT_OF_ARGUMENTS,
-	inputArguments_VALUE_THREAD_NOT_NUMBER,
-	inputArguments_WRONG_COUNT_THREAD,
-	inputArguments_VALUE_INTERATIONS_NOT_NUMBER,
-	inputArguments_WRONG_COUNT_ITERATIONS
+    inputArguments_SUCCESS,
+    inputArguments_WRONG_COUNT_OF_ARGUMENTS,
+    inputArguments_VALUE_THREAD_NOT_NUMBER,
+    inputArguments_WRONG_COUNT_THREAD,
+    inputArguments_VALUE_INTERATIONS_NOT_NUMBER,
+    inputArguments_WRONG_COUNT_ITERATIONS
 };
 
 int parsingInputArguments(int argc, char** argv, inputArguments* arguments) {
-	if (argc != RIGHT_NUMBER_ARGUMENTS) {
-		return inputArguments_WRONG_COUNT_OF_ARGUMENTS;
-	}
+    if (argc != RIGHT_NUMBER_ARGUMENTS) {
+        return inputArguments_WRONG_COUNT_OF_ARGUMENTS;
+    }
 
-	int countThread = atoi(argv[1]);
-	if (countThread == STRING_TO_INT_FAILURE && strcmp(argv[1], "0") != COMPARISON_SUCCESS) {
-		return inputArguments_VALUE_THREAD_NOT_NUMBER;
-	}
-	if (countThread < 0 || countThread > MAX_THREAD) {
-		return inputArguments_WRONG_COUNT_THREAD;
-	}
-	arguments->countThread = countThread;
+    int countThread = atoi(argv[1]);
+    if (countThread == STRING_TO_INT_FAILURE && strcmp(argv[1], "0") != COMPARISON_SUCCESS) {
+        return inputArguments_VALUE_THREAD_NOT_NUMBER;
+    }
+    if (countThread < 0 || countThread > MAX_THREAD) {
+        return inputArguments_WRONG_COUNT_THREAD;
+    }
+    arguments->countThread = countThread;
 
-	int countIterations = atoi(argv[2]);
-	if (countIterations == STRING_TO_INT_FAILURE && strcmp(argv[2], "0") != COMPARISON_SUCCESS) {
-		return inputArguments_VALUE_INTERATIONS_NOT_NUMBER;
-	}
-	if (countIterations < 0 || countIterations > MAX_ITERATIONS) {
-		return inputArguments_WRONG_COUNT_ITERATIONS;
-	}
-	arguments->countIterations = countIterations;
+    int countIterations = atoi(argv[2]);
+    if (countIterations == STRING_TO_INT_FAILURE && strcmp(argv[2], "0") != COMPARISON_SUCCESS) {
+        return inputArguments_VALUE_INTERATIONS_NOT_NUMBER;
+    }
+    if (countIterations < 0 || countIterations > MAX_ITERATIONS) {
+        return inputArguments_WRONG_COUNT_ITERATIONS;
+    }
+    arguments->countIterations = countIterations;
 
-	return inputArguments_SUCCESS;
+    return inputArguments_SUCCESS;
 }
 
 void inputError(int code) {
-	switch (code) {
-	case inputArguments_WRONG_COUNT_OF_ARGUMENTS:
-		fprintf(stderr, "introduced a wrong number of arguments\n");
-		break;
-	case inputArguments_VALUE_THREAD_NOT_NUMBER:
-		fprintf(stderr, "thread argument is not an number\n");
-		break;
-	case inputArguments_WRONG_COUNT_THREAD:
-		fprintf(stderr, "number of threads entered incorrectly\n");
-		break;
-	case inputArguments_VALUE_INTERATIONS_NOT_NUMBER:
-		fprintf(stderr, "iteration argument is not an number\n");
-		break;
-	case inputArguments_WRONG_COUNT_ITERATIONS:
-		fprintf(stderr, "number of iterations entered incorrectly\n");
-		break;
-	}
-	exit(EXIT_FAILURE);
+    switch (code) {
+    case inputArguments_WRONG_COUNT_OF_ARGUMENTS:
+        fprintf(stderr, "introduced a wrong number of arguments\n");
+        break;
+    case inputArguments_VALUE_THREAD_NOT_NUMBER:
+        fprintf(stderr, "thread argument is not an number\n");
+        break;
+    case inputArguments_WRONG_COUNT_THREAD:
+        fprintf(stderr, "number of threads entered incorrectly\n");
+        break;
+    case inputArguments_VALUE_INTERATIONS_NOT_NUMBER:
+        fprintf(stderr, "iteration argument is not an number\n");
+        break;
+    case inputArguments_WRONG_COUNT_ITERATIONS:
+        fprintf(stderr, "number of iterations entered incorrectly\n");
+        break;
+    }
+    exit(EXIT_FAILURE);
 }
 
 void divisionIteration(int* array, inputArguments args) {
-	int countInterartionsInOneThread = args.countIterations / args.countThread;
-	for (int i = 0; i < args.countThread; ++i) {
-		array[i] = countInterartionsInOneThread;
-	}
+    int countInterartionsInOneThread = args.countIterations / args.countThread;
+    for (int i = 0; i < args.countThread; ++i) {
+        array[i] = countInterartionsInOneThread;
+    }
 
-	int restIteration = args.countIterations % args.countThread;
-	if (restIteration != 0)
-		for (int i = 0; i < restIteration; ++i) {
-			++array[i];
-		}
+    int restIteration = args.countIterations % args.countThread;
+    if (restIteration != 0)
+        for (int i = 0; i < restIteration; ++i) {
+            ++array[i];
+        }
 }
 
 void fillingGeneralArrayForFunctionInThread(argumentsForFunctionInThread* array, inputArguments args, int* offset) {
-	for (int i = 0; i < args.countThread; ++i) {
-		if (i == 0) {
-			array[i].startIteration = 0;
-			array[i].endIteration = offset[i];
-		}
-		else {
-			array[i].startIteration = array[i - 1].endIteration;
-			array[i].endIteration = array[i - 1].endIteration + offset[i];
-		}
-		array[i].partialSum = 0.0;
-	}
+    for (int i = 0; i < args.countThread; ++i) {
+        if (i == 0) {
+            array[i].startIteration = 0;
+            array[i].endIteration = offset[i];
+        }
+        else {
+            array[i].startIteration = array[i - 1].endIteration;
+            array[i].endIteration = array[i - 1].endIteration + offset[i];
+        }
+        array[i].partialSum = 0.0;
+    }
 }
 
 void processingThreadError(int valueError, const char* msg) {
-	fprintf(stderr, "%s cause : %s\n", msg, strerror(valueError));
-	exit(EXIT_FAILURE);
+    fprintf(stderr, "%s cause : %s\n", msg, strerror(valueError));
+    exit(EXIT_FAILURE);
 }
 
 void* calculatePartialSum(void* args) {
-	argumentsForFunctionInThread* value = (argumentsForFunctionInThread*)args;
-	for (int i = value->startIteration; i < value->endIteration; ++i) 
-		if (i % 2 == 0) {
-			value->partialSum += 1.0 / (i * 2.0 + 1.0);
-		}
-		else {
-			value->partialSum -= 1.0 / (i * 2.0 + 1.0);
-		}
-	}
-	pthread_exit(&value->partialSum);
+    argumentsForFunctionInThread* value = (argumentsForFunctionInThread*)args;
+    for (int i = value->startIteration; i < value->endIteration; ++i) 
+        if (i % 2 == 0) {
+            value->partialSum += 1.0 / (i * 2.0 + 1.0);
+        }
+        else {
+            value->partialSum -= 1.0 / (i * 2.0 + 1.0);
+        }
+    }
+    pthread_exit(&value->partialSum);
 }
 
 void createThread(pthread_t* ntid, argumentsForFunctionInThread* array, inputArguments args) {
-	for (int i = 0; i < args.countThread; ++i) {
-		int err = pthread_create(&ntid[i], NULL, calculatePartialSum, (void*)&array[i]);
-		if (err != CREATE_SUCCESS) {
-			processingThreadError(err, "unable to create thread");
-		}
-	}
+    for (int i = 0; i < args.countThread; ++i) {
+        int err = pthread_create(&ntid[i], NULL, calculatePartialSum, (void*)&array[i]);
+        if (err != CREATE_SUCCESS) {
+            processingThreadError(err, "unable to create thread");
+        }
+    }
 }
 
 void collectionPartialSumm(pthread_t* ntid, argumentsForFunctionInThread* array, inputArguments args, double* sum) {
-	*sum = 0;
-	void* resultInThread;
-	for (int i = 0; i < args.countThread; ++i) {
-		int err = pthread_join(ntid[i], &resultInThread);
-		if (err != JOIN_SUCCESS) {
-			processingThreadError(err, "error while waiting for thread");
-		}
-		*sum += *(double*)resultInThread;
-	}
-	*sum *= 4;
+    *sum = 0;
+    void* resultInThread;
+    for (int i = 0; i < args.countThread; ++i) {
+        int err = pthread_join(ntid[i], &resultInThread);
+        if (err != JOIN_SUCCESS) {
+            processingThreadError(err, "error while waiting for thread");
+        }
+        *sum += *(double*)resultInThread;
+    }
+    *sum *= 4;
 }
 
 void printPi(double* pi) {
-	fprintf(stdout, "pi = %.20f\n", *pi);
+    fprintf(stdout, "pi = %.20f\n", *pi);
 }
 
 void calculationPI(inputArguments args) {
-	int countIterationInOneThread[args.countThread];
-	divisionIteration(countIterationInOneThread, args);
+    int countIterationInOneThread[args.countThread];
+    divisionIteration(countIterationInOneThread, args);
 
-	argumentsForFunctionInThread arrayArguments[args.countThread];
-	fillingGeneralArrayForFunctionInThread(arrayArguments, args, countIterationInOneThread);
+    argumentsForFunctionInThread arrayArguments[args.countThread];
+    fillingGeneralArrayForFunctionInThread(arrayArguments, args, countIterationInOneThread);
 
-	pthread_t ntid[args.countThread];
-	createThread(ntid, arrayArguments, args);
+    pthread_t ntid[args.countThread];
+    createThread(ntid, arrayArguments, args);
 
-	double pi = 0;
-	collectionPartialSumm(ntid, arrayArguments, args, &pi);
-	printPi(&pi);
+    double pi = 0;
+    collectionPartialSumm(ntid, arrayArguments, args, &pi);
+    printPi(&pi);
 }
 
 int main(int argc, char** argv) {
-	inputArguments args;
+    inputArguments args;
 
-	int code = parsingInputArguments(argc, argv, &args);
-	if (code != inputArguments_SUCCESS) {
-		inputError(code);
-	}
+    int code = parsingInputArguments(argc, argv, &args);
+    if (code != inputArguments_SUCCESS) {
+        inputError(code);
+    }
 
-	calculationPI(args);
+    calculationPI(args);
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
