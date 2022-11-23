862d18ae307c883ab4084ecb380a7a9a9dc3ffa7
marina <marina.demchuk.01@bk.ru>
2022-11-23 13:34:27 +0700

    add lab6

--- lab6/lab6/lab6.c
+++ lab6/lab6/lab6.c
@@ -27,7 +27,7 @@ void* printLine(void* args) {
 }
 
 int readLines(argumetsForFunctionInThread* array) {
-    printf("Enter lines, please! Or enter 'end' for completions programm!\nMaximum number of rows = 100, Maximum string length = 100\n");
+    printf("Enter lines, please! Or enter 'end' for completions programm!\nMaximum number of rows = 100\n");
     while (fgets(array[countThread].text, MAX_LENGTH_LINE - 1, stdin) != NULL) {
         if (strcmp(array[countThread].text, "end\n") == 0 || countThread == MAX_QUANTITY_LINES) {
             break;
